2486334757919d75614a9f814b0b5bd85dddda30
Loug <lucianoguasque@gmail.com>
2025-04-22 05:19:29 -0300

    Short session today, next step is to learn more about dealing with lists.

--- dayTwentyOne/Lists.java
+++ dayTwentyOne/Lists.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class Lists {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Hello");
+        list.add(",");
+        list.add("World!");
+
+        System.out.println(list.get(2));
     }
 }
