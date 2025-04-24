15cdc18c832676e7c750d58f4c084b5b02f81838
Loug <lucianoguasque@gmail.com>
2025-04-23 21:06:59 -0300

    End of a short session, will resume in a few hours.

--- dayTwentyOne/Lists.java
+++ dayTwentyOne/Lists.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Lists {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        list.add("Hello");
-        list.add(",");
-        list.add("World!");
+        while (true) {
+            System.out.print("Type a number (0 to quit): ");
+            int userInput = Integer.valueOf(scanner.nextLine());
+            list.add(userInput);
+            
+            if (userInput == 0) {
+                list.removeLast();
+                break;
+            }
+        }
 
-        System.out.println(list.get(2));
+        for (int i = 0; i < list.size(); i++) {;
+            System.out.print(list.get(i) + " ");
+        }
     }
 }
