3164cfb1a1f765b5fd401776caa3cd1d08299e92
Loug <lucianoguasque@gmail.com>
2025-04-24 03:10:01 -0300

    recoded addToList into a class of its own

--- dayTwentyOne/Lists.java
+++ dayTwentyOne/Lists.java
@@ -3,30 +3,73 @@ import java.util.Scanner;
 
 public class Lists {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        String myStr = "";
-        int sum = 0;
+        AddToList addToList = new AddToList();
+        addToList.addTolist();
+        System.out.println(addToList.findSum());
+        System.out.println(addToList.findSmallest());
+    }
+}
 
+// TODO find a way to implement the smallest method into the Smallest class
+/* class Smallest extends AddToList{
+    ArrayList<Integer> smallestArr = new ArrayList<>();
+    int smallest = numArr.get(0);
+
+    public void small() {
+        for (int i = 0; i < smallestArr.size(); i++) {
+            int num = smallestArr.get(i);
+            if (smallest > num) {
+                smallest = num;
+            }
+        }
+    }
+} */
+
+class AddToList {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> numArr = new ArrayList<>(); 
+    String finalStr = "";
+    int sum = 0;
+    
+    public void addTolist() {
         while (true) {
-            System.out.print("Type a number (0 to quit): ");
-            int userInput = Integer.valueOf(scanner.nextLine());
-            sum += userInput;
-            list.add(userInput);
+            System.out.print("Say a number (0 to quit): ");            
+            int userInput = Integer.valueOf(scan.nextLine());
 
             if (userInput != 0) {
-                System.out.println("Added " + userInput + " to the list.");
+                numArr.add(userInput);
+                System.out.println(userInput + " was added to the list.");
             }
-
+            
             if (userInput == 0) {
-                list.removeLast();
                 break;
             }
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            myStr += list.get(i) + " ";
+        for (int i = 0; i < numArr.size(); i++) {
+            finalStr += numArr.get(i) + " and "; // TODO learn how to remove the last " and "
         }
-        System.out.println("The sum of " + myStr + "is " + sum);
+    }
+
+    public String findSum() {
+        for (Integer integer : numArr) {
+            sum += integer;            
+        }
+
+        // System.out.println("The sum of " + finalStr + "is " + sum);
+        return "The sum of " + finalStr + "is " + sum;
+    }
+
+    public String findSmallest() {
+        int smallest = numArr.get(0);
+
+        for (int i = 0; i < numArr.size(); i++) {
+            int num = numArr.get(i);
+
+            if (smallest > num) {
+                smallest = num;
+            }
+        }
+        return "The smallest number was " + smallest;
     }
 }
