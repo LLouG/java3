0199b39d140e707acfc864021098989eccab02e6
Loug <lucianoguasque@gmail.com>
2025-04-28 06:05:04 -0300

    I keep going back to the Person class and still struggling a bit on the getters and setters TODO: reserve a day to work on this subject

--- dayTwentyFour/ListObj.java
+++ dayTwentyFour/ListObj.java
@@ -1,7 +1,37 @@
 package dayTwentyFour;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ListObj {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Person> persons = new ArrayList<>();
+
+        System.out.println("Enter name and age separated by a comma, e.g.: Loug,32 ");
+        while (true) {
+            System.out.print("Enter the details, empty will stop: ");
+            String details = scanner.nextLine();
+            if (details.isEmpty()) {
+                break;
+            }
+
+            String[] parts = details.split(",");
+            String name = parts[0];
+            int age = Integer.valueOf(parts[1]);
+            persons.add(new Person(name, age));
+        }
         
+        System.out.println();        
+        System.out.println("Total of names: " + persons.size());
+        System.out.println("Persons: ");
+        
+        System.out.println("What is the minimum age?");
+        int minAge = Integer.valueOf(scanner.nextLine());
+        for (Person person : persons) {
+            if(person.getAge() >= minAge){
+                System.out.println(person);
+            }
+        }
     }
 }
---
+++ dayTwentyFour/Person.java
@@ -0,0 +1,32 @@
+package dayTwentyFour;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // setters are required here, otherwise it will print the location in the memory instead
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String toString() {
+        return this.name + ", age: " + this.age;
+    }
+}
