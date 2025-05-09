c256ef66a9be3043ab8450b08f39e2110f13bf63
Loug <lucianoguasque@gmail.com>
2025-05-08 22:17:35 -0300

    small edit

--- dayThirty/App.java
+++ dayThirty/App.java
@@ -3,5 +3,35 @@ package dayThirty;
 public class App {
     public static void main(String[] args) {
         System.out.println("Time for some exercises!");
+
+        Age first = new Age(32);
+        System.out.println(first);
+        first.growOlder();
+        System.out.println(first);
+
+        Age second = first;
+        second.growOlder();
+        System.out.println(first);
+    }
+}
+
+
+class Age {
+    private int age;
+
+    public Age(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void growOlder() {
+        this.age++;
+    }
+
+    public String toString() {
+        return "[" + this.age + "]";
     }
 }
