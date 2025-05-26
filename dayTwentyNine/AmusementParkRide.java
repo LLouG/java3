edb08f57e97227204e5b421038005a6336ab9cd9
Loug <lucianoguasque@gmail.com>
2025-05-25 23:33:11 -0300

    added a visitors counter

--- dayTwentyNine/AmusementParkRide.java
+++ dayTwentyNine/AmusementParkRide.java
@@ -5,20 +5,23 @@ import dayTwentyThree.Person;
 public class AmusementParkRide {
     private String name;
     private int lowestHeight;
+    private int visitors;
     
     public AmusementParkRide(String name, int lowestHeight) {
         this.name = name;
         this.lowestHeight = lowestHeight;
+        this.visitors = 0;
     }
 
     public Boolean allowedToRide(Person person) {
         if (person.getHeight() < lowestHeight) {
             return false;
         }
+        visitors++;
         return true;        
     }
 
     public String toString() {
-        return name + ", minimum height: " + lowestHeight;
+        return "Ride name: " + name + "\nMinimum height: " + lowestHeight + "\nVisitors today: " + visitors;
     }
 }
